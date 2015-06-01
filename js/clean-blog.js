3d9ba0b8d5cb30f5384726927114bd6a277e5692
余鸿飞 <yuhongfei@hupu.com>
2015-06-01 18:34:54 +0800

    fix code

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -31,7 +31,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+		url: "http://120.26.62.213/mail.php",
                 type: "POST",
                 data: {
                     name: name,
